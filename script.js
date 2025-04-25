a989e579fec8fe72c5558c9cd4b04180141d8e9b
BaharehNorouzi <baharehnorouzi85@gmail.com>
2025-04-25 16:16:47 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let agerange= prompt("please enter your age");
+if(agerange>=10 && agerange<=20){
+    alert("you are teenager");
+}
+else if(agerange>=20 && agerange<=40){
+    alert("you are young");
+}
+else{
+    alert("you are adult");
+}
