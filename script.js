c88f1dde6d375aa8b320cb8ab422a9dd291c7697
BaharehNoruzi <baharehnorouzi85@gmail.com>
2025-04-25 16:25:21 +0330

    add a uname and pass in java

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let agerange= prompt("please enter your age");
-if(agerange>=10 && agerange<=20){
+let uname = prompt("please enter your username:");
+let pass = prompt("please enter your password:");
+if (uname === "admin" && pass === "123") {
+    alert("welcom");
+} else {
+    alert("wrong username or password");
+}
+
+
+let agerange = prompt("please enter your age");
+if (agerange >= 10 && agerange <= 20) {
     alert("you are teenager");
 }
-else if(agerange>=20 && agerange<=40){
+else if (agerange >= 20 && agerange <= 40) {
     alert("you are young");
 }
-else{
+else {
     alert("you are adult");
 }
